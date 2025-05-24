93a7c7124a7e908305a0a9318b7c1a7bf1f56c41
Tyrone <1014990573@qq.com>
2025-05-24 18:57:41 +0800

    feat(basic): v1 done
    (cherry picked from commit 4d60bd454e481da8f4b50327b0970e3dc0701daa)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,61 @@
-<script setup>
-import AppFooter from "./components/AppFooter.vue";
-import AppTopbar from "./components/AppTopbar.vue";
-import StatsWidget from "./components/dashboard/StatsWidget.vue";
-import SalesTrendWidget from "./components/dashboard/SalesTrendWidget.vue";
-import RecentActivityWidget from "./components/dashboard/RecentActivityWidget.vue";
-import ProductOverviewWidget from "./components/dashboard/ProductOverviewWidget.vue";
+<template>
+  <div class="min-h-screen p-8 flex flex-col  flex-1 gap-6"
+       :style="`background-color: ${backgroundColor}`"
+  >
+    <div class="flex flex-col w-full max-w-7xl mx-auto gap-6">
+      <AvatarSelector @update:background="handleBackgroundUpdate"></AvatarSelector>
+    </div>
+  </div>
+</template>
+
+<script>
 import AvatarSelector from "./components/AvatarSelector.vue";
+
+export default {
+  components: {
+    AvatarSelector
+  },
+
+  data() {
+    return {
+      backgroundColor: '#80deea'
+    }
+  },
+  methods: {
+    handleBackgroundUpdate(backgroundColor) {
+      if (backgroundColor === 'transparent') {
+        this.backgroundColor = '#80deea';
+        return;
+      }
+      this.backgroundColor = this.generateSimilarColor(backgroundColor);
+    },
+
+    generateSimilarColor(color) {
+      const hexToRgb = (hex) => {
+        const bigint = parseInt(hex.slice(1), 16);
+        return {
+          r: (bigint >> 16) & 255,
+          g: (bigint >> 8) & 255,
+          b: bigint & 255,
+        };
+      };
+      const rgbToHex = ({r, g, b}) =>
+          `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+      const adjustColor = (value) => Math.max(0, Math.round(value - 20));
+      const rgb = hexToRgb(color);
+      const darkerRgb = {
+        r: adjustColor(rgb.r),
+        g: adjustColor(rgb.g),
+        b: adjustColor(rgb.b),
+      };
+      return rgbToHex(darkerRgb);
+    },
+
+  },
+
+  mounted() {
+  }
+}
 </script>
 
-<template>
-    <div class="bg-blue-100 dark:bg-surface-950 min-h-screen p-8 flex flex-col gap-6">
 
-        <AppTopbar />
-        <div class="flex flex-col w-full max-w-7xl mx-auto gap-6 flex-1">
-          <AvatarSelector></AvatarSelector>
-        </div>
-        <AppFooter />
-    </div>
-</template>
